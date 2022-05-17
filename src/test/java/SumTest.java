da798599261d3f475c3dc18306227b273b8926a2
JiKwang <wlrhkd49@naver.com>
2022-05-17 21:21:48 +0900

    junit5 & 대역

---
+++ src/test/java/SumTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SumTest {
+
+    @Test
+    void sum() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+
+}
